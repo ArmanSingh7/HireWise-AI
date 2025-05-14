47d67b729aa29b2b9cb91ae80af4179987f01788
ArmanSingh7 <businessapp7781@gmail.com>
2025-05-14 17:27:14 +0530

    Switched Hash

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/HireWise-AI/', // 👈 Important: Matches your GitHub repo name
 })
