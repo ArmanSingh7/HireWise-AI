933138766b716c779a12d45906e283699080f76f
ArmanSingh7 <businessapp7781@gmail.com>
2025-05-14 15:49:35 +0530

    Initial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import HERO_IMG from "../assets/hero-img.png";
+import { APP_FEATURES } from "../utils/data";
+import {LuSparkles} from 'react-icons/lu'
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {};
+
+  return (
+    <>
+<div className="w-full min-h-full bg-[#FFFCEF] px-4 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32">
+<div className="w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left-0" />
+
+      <div className="container mx-auto px-4 pt-6 pb-[200px] relative z-10">
+        {/*Header*/}
+        <header className="flex justify-between items-center mb-16">
+          <div className="text-xl text-black font-bold">
+            Interview Prep AI 
+          </div>
+            <button 
+            className="bg-linear-to-r from-[#FF9324] TO-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
+            onClick={() => setOpenAuthModal(true)}
+            >
+              Login / Sign Up
+            </button>
+        </header>
+
+            {/*Hero Content*/}
+            <div className="flex flex-col md:flex-row items-center">
+               <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+                 <div className="flex items-center justify-left mb-2">
+                    <div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
+                     <LuSparkles/>AI Powered
+                    </div>
+                  </div>
+
+                  <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
+                    Ace Interviews with <br />
+                    <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                      AI-Powered
+                    </span>{" "}
+                    Learning
+                  </h1>
+               </div>
+
+                  <div className="w-full md:w-1/2" >
+                    <p className="text-[17px] text-black-900 mr-0 md:mr-20 mb-6">
+                      Get role-specific questions, expand answers when you need them,
+                      dive deeper into concepts, and organize everything your way.
+                      From preparation to mastery - your ultimate interview toolkit is here.
+                    </p>
+
+
+                    <button 
+                    className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer"
+                    onClick={handleCTA}
+                    >
+                      Get Started
+                    </button>
+
+                  </div>
+                </div>
+            </div>
+      </div>
+      <div className="w-full min-h-full relative z-10 mb-56">
+          <div>
+              <section className="flex item-center justify-center -mt-36">
+                  <img 
+                    src={HERO_IMG}
+                    alt="Hero Image"
+                    className="w-[80vw] rounded-lg"
+                    />
+                  </section>
+            </div>
+
+         <div className="">
+            <div className="">
+               <section className="">
+                  <h2 className="">
+                    Features That Makes You Shine
+                  </h2>
+
+                  <div className="">
+                    {/*First 3 cards*/}
+                      <div className="">
+                          {APP_FEATURES.slice(0,3).map((feature)=>(
+                            <div 
+                            key={feature.id}
+                            className=""
+                            >
+                              <h3 className="">
+                                {feature.title}
+                              </h3>
+                              <p className="">{feature.description}</p>
+                              </div>
+
+                          ))}
+                          </div>
+                          {/*Remaining 2 cards*/}
+                          <div className="">
+                          {APP_FEATURES.slice(3).map((feature)=>(
+                            <div 
+                            key={feature.id}
+                            className=""
+                            >
+                              <h3 className="">
+                                {feature.title}
+                              </h3>
+                              <p className="">{feature.description}</p>
+                              </div>
+                          ))}
+                          </div>
+                      </div>
+                    </section>
+              </div>
+              </div>
+
+         <div className="">
+          Made with ❤️.. Happy coding
+          </div>
+
+
+
+
+        </div>
+
+    </>
+         
+  )
+}
+
+export default LandingPage
